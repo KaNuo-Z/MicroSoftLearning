7cfb25189ea5370aa0c6f2d0d5f7d97a900f5d3e
章龙 <52533297@qq.com>
2020-06-03 17:45:48 +0800

    IdentityServer测试项目

---
+++ WebClient/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using IdentityServer4.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebClient.Controllers
+{
+    [Authorize]
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.UserName = HttpContext.User?.GetDisplayName();
+            return View();
+        }
+
+        public IActionResult LogOut()
+        {
+            return SignOut("Cookies", "oidc");
+        }
+    }
+}
